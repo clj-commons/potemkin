81836ae572fb60d3f6ea4bd748e93d81ec9576d6
prospero <ztellman@gmail.com>
2010-09-04 12:46:27 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject potemkin "0.1.0"
+  :description "Utility functions for managing code complexity"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ src/potemkin.clj
@@ -0,0 +1,21 @@
+;;   Copyright (c) Zachary Tellman. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
+(ns potemkin)
+
+(defmacro import-fn 
+  "Given a function in another namespace, defines a function by
+   the same name in the current namespace.  Argument lists and
+   doc-strings are preserved."
+  [sym]
+  (let [m (meta (eval sym))
+        m (meta (intern (:ns m) (:name m)))
+        n (:name m)
+        arglists (:arglists m)
+        doc (:doc m)]
+    (list `def (with-meta n {:doc doc :arglists (list 'quote arglists)}) (eval sym))))
