1b998cd9b5db52f10a4f3aa8d841ac5e02f76ed9
ztellman <ztellman@gmail.com>
2011-10-21 19:04:07 -0700

    update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject potemkin "0.1.1-SNAPSHOT"
   :description "Some useful facades."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
