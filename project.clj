288cd4d2d6537816918b6cbebd32e0b6ac11a196
Zach Tellman <ztellman@gmail.com>
2012-12-25 17:52:00 -0800

    target the 1.5.0 snapshot, instead

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-rc1"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
   :aliases {"all" ["with-profile" "1.2,dev:1.3,dev:dev:1.5,dev"]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
