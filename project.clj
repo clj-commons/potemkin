2d29bcefd0c651ce899848e0c5724666cc43dd24
Zach Tellman <ztellman@gmail.com>
2013-03-24 22:56:14 -0700

    mark 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject potemkin "0.2.1-SNAPSHOT"
+(defproject potemkin "0.2.1"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
