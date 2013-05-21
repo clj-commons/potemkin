80417882157b0faf537bd565b335c90a6f80ca49
Lee Hinman <lee@writequit.org>
2013-05-21 09:12:53 -0600

    Allow specifying an optional name for import-fn and import-macro

--- src/potemkin/namespaces.clj
+++ src/potemkin/namespaces.clj
@@ -9,60 +9,68 @@
 (ns potemkin.namespaces
   (:use [clojure pprint]))
 
-(defmacro import-fn 
-  "Given a function in another namespace, defines a function with the same name in the
-   current namespace.  Argument lists, doc-strings, and original line-numbers are preserved."
-  [sym]
-  (let [vr (resolve sym)
-        m (meta vr)
-        n (:name m)
-        arglists (:arglists m)
-        doc (:doc m)
-        protocol (:protocol m)]
-    (when-not vr
-      (throw (IllegalArgumentException. (str "Don't recognize " sym))))
-    (when (:macro m)
-      (throw (IllegalArgumentException. (str "Calling import-fn on a macro: " sym))))
-    `(do
-       (def ~(with-meta n {:protocol protocol}) (deref ~vr))
-       (alter-meta! (var ~n) assoc
-         :doc ~doc
-         :arglists ~(list 'quote arglists)
-         :file ~(:file m)
-         :line ~(:line m))
-       ~vr)))
+(defmacro import-fn
+  "Given a function in another namespace, defines a function with the
+   same name in the current namespace.  Argument lists, doc-strings,
+   and original line-numbers are preserved."
+  ([sym]
+     `(import-fn ~sym nil))
+  ([sym name]
+     (let [vr (resolve sym)
+           m (meta vr)
+           n (or name (:name m))
+           arglists (:arglists m)
+           doc (:doc m)
+           protocol (:protocol m)]
+       (when-not vr
+         (throw (IllegalArgumentException. (str "Don't recognize " sym))))
+       (when (:macro m)
+         (throw (IllegalArgumentException.
+                 (str "Calling import-fn on a macro: " sym))))
+       `(do
+          (def ~(with-meta n {:protocol protocol}) (deref ~vr))
+          (alter-meta! (var ~n) assoc
+                       :doc ~doc
+                       :arglists ~(list 'quote arglists)
+                       :file ~(:file m)
+                       :line ~(:line m))
+          ~vr))))
 
 (defmacro import-macro
-  "Given a macro in another namespace, defines a macro with the same name in the
-   current namespace.  Argument lists, doc-strings, and original line-numbers are preserved."
-  [sym]
-  (let [vr (resolve sym)
-        m (meta vr)
-        n (:name m)
-        arglists (:arglists m)
-        doc (:doc m)]
-    (when-not vr
-      (throw (IllegalArgumentException. (str "Don't recognize " sym))))
-    (when-not (:macro m)
-      (throw (IllegalArgumentException. (str "Calling import-macro on a non-macro: " sym))))
-    `(do
-       (def ~n ~(resolve sym))
-       (alter-meta! (var ~n) assoc
-         :doc ~doc
-         :arglists ~(list 'quote arglists)
-         :file ~(:file m)
-         :line ~(:line m))
-       (.setMacro (var ~n))
-       ~vr)))
+  "Given a macro in another namespace, defines a macro with the same
+   name in the current namespace.  Argument lists, doc-strings, and
+   original line-numbers are preserved."
+  ([sym]
+     `(import-macro ~sym nil))
+  ([sym name]
+     (let [vr (resolve sym)
+           m (meta vr)
+           n (or name (:name m))
+           arglists (:arglists m)
+           doc (:doc m)]
+       (when-not vr
+         (throw (IllegalArgumentException. (str "Don't recognize " sym))))
+       (when-not (:macro m)
+         (throw (IllegalArgumentException.
+                 (str "Calling import-macro on a non-macro: " sym))))
+       `(do
+          (def ~n ~(resolve sym))
+          (alter-meta! (var ~n) assoc
+                       :doc ~doc
+                       :arglists ~(list 'quote arglists)
+                       :file ~(:file m)
+                       :line ~(:line m))
+          (.setMacro (var ~n))
+          ~vr))))
 
-(defmacro import-def 
+(defmacro import-def
   "Given a regular def'd var from another namespace, defined a new var with the
    same name in the current namespace."
   [sym]
   (let [vr (resolve sym)
         m (meta vr)
         n (:name m)
-        n (if (:dynamic m) (with-meta n {:dynamic true}) n) 
+        n (if (:dynamic m) (with-meta n {:dynamic true}) n)
         nspace (:ns m)
         doc (:doc m)]
     (when-not vr
@@ -70,9 +78,9 @@
     `(do
        (def ~n @~vr)
        (alter-meta! (var ~n) assoc
-         :doc ~doc
-         :file ~(:file m)
-         :line ~(:line m))
+                    :doc ~doc
+                    :file ~(:file m)
+                    :line ~(:line m))
        ~vr)))
 
 (defmacro import-vars
@@ -81,23 +89,23 @@
   (let [unravel (fn unravel [x]
                   (if (sequential? x)
                     (->> x
-                      rest
-                      (mapcat unravel)
-                      (map
-                        #(symbol
-                           (str (first x)
-                             (when-let [n (namespace %)]
-                               (str "." n)))
-                           (name %))))
+                         rest
+                         (mapcat unravel)
+                         (map
+                          #(symbol
+                            (str (first x)
+                                 (when-let [n (namespace %)]
+                                   (str "." n)))
+                            (name %))))
                     [x]))
         syms (mapcat unravel syms)]
     `(do
        ~@(map
-           (fn [sym]
-             (let [vr (resolve sym)
-                   m (meta vr)]
-               (cond
-                 (:macro m) `(import-macro ~sym)
-                 (:arglists m) `(import-fn ~sym)
-                 :else `(import-def ~sym))))
-           syms))))
+          (fn [sym]
+            (let [vr (resolve sym)
+                  m (meta vr)]
+              (cond
+               (:macro m) `(import-macro ~sym)
+               (:arglists m) `(import-fn ~sym)
+               :else `(import-def ~sym))))
+          syms))))
