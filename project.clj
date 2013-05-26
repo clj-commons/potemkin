715e461a330e58ff878c6989a3364b8bb2a21c87
Zach Tellman <ztellman@gmail.com>
2013-05-25 22:00:28 -0700

    update to MIT license, update readme, move to 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject potemkin "0.2.3-SNAPSHOT"
+(defproject potemkin "0.3.0-SNAPSHOT"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
--- src/potemkin/types.clj
+++ src/potemkin/types.clj
@@ -1,11 +1,3 @@
-;;   Copyright (c) Zachary Tellman. All rights reserved.
-;;   The use and distribution terms for this software are covered by the
-;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;;   which can be found in the file epl-v10.html at the root of this distribution.
-;;   By using this software in any fashion, you are agreeing to be bound by
-;;   the terms of this license.
-;;   You must not remove this notice, or any other, from this software.
-
 (ns potemkin.types
   (:use
     [clojure walk [set :only (union)]]
@@ -121,7 +113,7 @@
   [name & body]
   `(def
      ~(with-meta name {:tag :potemkin/abstract-type})
-     '(deftype ~name ~@body)))
+     '(deftype ~name [] ~@body)))
 
 (defmacro defprotocol+
   "A protocol that won't evaluate if an equivalent protocol with the same name already exists."
