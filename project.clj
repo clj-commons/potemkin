01e5ac3540af093e3ce4cf1ca12c4d9f2310e913
Zach Tellman <ztellman@gmail.com>
2013-10-30 19:13:33 -0700

    update readme

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :license {:name "MIT License"}
   :description "Some useful facades."
   :dependencies [[clj-tuple "0.1.2"]
-                 [riddley "0.1.5"]]
+                 [riddley "0.1.6"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [criterium "0.4.1"]
                                   [collection-check "0.1.1-SNAPSHOT"]]}
