8f39702b7986b77901055f12f3f2b75ca2718f6a
Zach Tellman <ztellman@gmail.com>
2016-01-05 23:49:30 -0800

    target latest riddley, mark 0.4.3

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject potemkin "0.4.2"
+(defproject potemkin "0.4.3"
   :license {:name "MIT License"}
   :description "Some useful facades."
   :dependencies [[clj-tuple "0.2.2"]
-                 [riddley "0.1.11"]]
+                 [riddley "0.1.12"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [collection-check "0.1.6"]]}
-             :provided {:dependencies [[org.clojure/clojure "1.8.0-RC2"]]}}
+             :provided {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}}
   :global-vars {*warn-on-reflection* true}
   :test-selectors {:default #(not (some #{:benchmark}
                                         (cons (:tag %) (keys %))))
