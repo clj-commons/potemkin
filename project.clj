f1a13fd07e294413610a09d4eade92a46102bfa7
ztellman <ztellman@gmail.com>
2017-11-27 12:46:36 -0800

    mark 0.4.5-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject potemkin "0.4.5-SNAPSHOT"
+(defproject potemkin "0.4.5-alpha1"
   :license {:name "MIT License"}
   :description "Some useful facades."
   :dependencies [[clj-tuple "0.2.2"]
