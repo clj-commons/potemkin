ca49fbfc50d4c69599ddf4884d8e64c6511786b7
Matthew Davidson <matthew@modulolotus.net>
2022-08-10 00:06:12 +0800

    Upgrade project release 0.4.6
    Upgrade deps
    Set min Java version at 8

--- project.clj
+++ project.clj
@@ -1,17 +1,16 @@
-(defproject potemkin "0.4.5"
+(defproject potemkin "0.4.6"
   :license {:name "MIT License"}
   :description "Some useful facades."
   :dependencies [[clj-tuple "0.2.2"]
                  [riddley "0.1.12"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.3"]
+  :profiles {:dev {:dependencies [[criterium "0.4.6"]
                                   [collection-check "0.1.6"]]}
-             :provided {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}}
+             :provided {:dependencies [[org.clojure/clojure "1.11.1"]]}}
   :global-vars {*warn-on-reflection* true}
   :test-selectors {:default #(not (some #{:benchmark}
                                         (cons (:tag %) (keys %))))
                    :benchmark :benchmark
                    :all (constantly true)}
   :java-source-paths ["src"]
-  :jvm-opts ^:replace ["-server" "-Xmx4g"]
-  :javac-options ["-source" "1.6" "-target" "1.6"]
+  :javac-options ["-source" "1.8" "-target" "1.8"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
