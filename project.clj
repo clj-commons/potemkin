4407a358eec50fd42bfdc6637f0b1624c7d3d2cf
Erik Assum <erik.assum@ardoq.com>
2023-11-23 11:47:36 +0100

    New version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject potemkin "0.4.6"
+(defproject potemkin "0.4.7"
   :license {:name "MIT License"}
   :description "Some useful facades."
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
